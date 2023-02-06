aa99f00ac3c04d6c9b400eca148d34e7c35a9a2b
PJ Linebaugh <pjl@renci.org>
2023-02-06 16:48:40 -0500

    initial set of files for helx initialization of pgadmin

--- start-pgadmin.sh
+++ start-pgadmin.sh
@@ -2,9 +2,13 @@
 
 export SCRIPT_NAME="$NB_PREFIX"
 export PGADMIN_CONFIG_DATA_DIR="\"/home/${USER_NAME}/.pgadmin_helx\""
-mkdir -p "$PGADMIN_CONFIG_DATA_DIR"
+
+# Do any HeLx-specific configurations.
+/helx-init.sh
+
 chown -R 5050:5050 "$PGADMIN_CONFIG_DATA_DIR"
 chmod 775 "/home/${USER_NAME}"
 chown 30000:5050 "/home/${USER_NAME}"
 
+# Start regular entrypoint of pgadmin.
 /entrypoint.sh
