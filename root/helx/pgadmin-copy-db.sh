6ca9da35d0ba42dcc4814a7fbc65a4d1965ef3cc
PJ Linebaugh <pjl@renci.org>
2023-02-06 19:46:26 -0500

    remove eduhelx-specific files

--- root/helx/pgadmin-copy-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -eoux pipefail
-
-if [ ! -f "$PGADMIN_CONFIG_DATA_DIR/pgadmin4.db" ]
-then
-  mkdir -p "$PGADMIN_CONFIG_DATA_DIR"
-  cp /helx/pgadmin4.db "$PGADMIN_CONFIG_DATA_DIR/"
-fi
