c24928831e64b1d1774a54ffa73e914bec7463aa
PJ Linebaugh <pjl@renci.org>
2023-02-06 19:40:34 -0500

    add scripts for build/push

---
+++ docker-config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Uncomment and set TAG if it should be the same for every build, 
+# otherwise a timestamp and "latest" will be used.
+# export TAG="0.0.4"
+export IMAGE_NAME=helxplatform/pgadmin4
+export REGISTRY=containers.renci.org
