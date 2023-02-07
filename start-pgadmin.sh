56840727b6bd8c92dcbb1e28dcba30029ce1a51a
PJ Linebaugh <pjl@renci.org>
2023-02-06 19:39:41 -0500

    added git ignore file

--- start-pgadmin.sh
+++ start-pgadmin.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 export SCRIPT_NAME="$NB_PREFIX"
-export PGADMIN_CONFIG_DATA_DIR="\"/home/${USER_NAME}/.helx/pgadmin\""
+export PGADMIN_CONFIG_DATA_DIR="/home/${USER_NAME}/.helx/pgadmin"
 
 # Do any HeLx-specific configurations.
 /helx-init.sh
 
-# chown -R 5050:5050 "$PGADMIN_CONFIG_DATA_DIR"
-# chmod 775 "/home/${USER_NAME}"
-# chown 30000:5050 "/home/${USER_NAME}"
+# pgAdmin wants quotes around a string variable name.
+export PGADMIN_CONFIG_DATA_DIR="\"$PGADMIN_CONFIG_DATA_DIR\""
 
 # Start regular entrypoint of pgadmin.
 /entrypoint.sh
